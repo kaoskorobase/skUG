f1ed634b3f377e1c35d78985cd2d6bfb609cf371
stefan kersten <sk@k-hornz.de>
2008-07-12 19:15:36 +0200

    site_scons/site_tools/faust.py: Merge in changes from skons
    darcs-hash:20080712171536-dde64-d09fa6132d49954c51ff9741760e36d28addb46b.gz

--- site_scons/site_tools/faust.py
+++ site_scons/site_tools/faust.py
@@ -40,46 +40,88 @@ import SCons.Action
 import SCons.Builder
 import SCons.Defaults
 import SCons.Environment
+import SCons.Scanner
 import SCons.Tool
 import SCons.Util
 
 FAUST = 'faust'
 INCLUDE_RE = re.compile(r'import\s*\(\s*"([^"]+)"\s*\)\s*;', re.M)
 
-def faust_scan(node, env, path):
-   contents = node.get_contents()
-   includes = INCLUDE_RE.findall(contents)
-   return includes
+def flatten(l):
+    r = []
+    for ll in l:
+        r += ll
+    return r
+
+def dsp_source_scanner(node, env, path):
+    """Scan source files for imported files in `path'."""
+    contents = node.get_contents()
+    includes = INCLUDE_RE.findall(contents)
+    path = [os.path.dirname(str(node))] + list(path)
+    return filter(os.path.exists, flatten(map(lambda f: map(lambda d: os.path.join(os.path.realpath(str(d)), f), path), includes)))
+
+def dsp_target_scanner(node, env, path):
+    """Search for architecture file in `path'."""
+    arch = env['FAUST_ARCHITECTURE'] + '.cpp'
+    return filter(os.path.exists, map(lambda d: os.path.join(str(d), arch), path))
+
+def svg_emitter(target, source, env):
+    target = target + map(lambda x: os.path.join(str(x), 'process.svg'), target)
+    print map(str, target)
+    return (target, source)
+    #return (target + [os.path.join(str(target[0]), 'process.svg')], source)
+
+def svg_scanner(node, env, path):
+    return [os.path.join(str(node), 'process.svg')]
 
 def generate(env):
     dsp = SCons.Builder.Builder(
-            action = 'faust -a ${FAUST_ARCHITECTURE}.cpp -o $TARGET $SOURCE',
+            action = 'faust ${FAUST_FLAGS} -a ${FAUST_ARCHITECTURE}.cpp -o $TARGET $SOURCE',
             suffix = '.cpp',
-            src_suffix = '.dsp')
+            src_suffix = '.dsp',
+            target_scanner = env.Scanner(
+                function = dsp_target_scanner,
+                path_function = SCons.Scanner.FindPathDirs('FAUST_PATH')))
     xml = SCons.Builder.Builder(
-            action = ['faust -o /dev/null -xml $SOURCE', SCons.Defaults.Move('$TARGET', '${SOURCE}.xml')],
+            action = ['faust ${FAUST_FLAGS} -o /dev/null -xml $SOURCE', SCons.Defaults.Move('$TARGET', '${SOURCE}.xml')],
             suffix = '.dsp.xml',
             src_suffix = '.dsp')
-    sc  = SCons.Builder.Builder(
-            action = '$FAUST2SC --prefix="${FAUST2SC_PREFIX}" -o $TARGET $SOURCE',
-            suffix = '.sc',
-            src_suffix = '.dsp.xml')
     svg = SCons.Builder.Builder(
-            action = ['faust -o /dev/null -svg $SOURCE', SCons.Defaults.Move('$TARGET', '${SOURCE}-svg')],
+            action = ['faust ${FAUST_FLAGS} -o /dev/null -svg $SOURCE', SCons.Defaults.Move('$TARGET', '${SOURCE}-svg')],
             suffix = '.dsp-svg',
             src_suffix = '.dsp',
-            target_factory = env.Dir)
+            single_source = True,
+            target_factory = env.Dir,
+            target_scanner = env.Scanner(function = svg_scanner))
+    sc  = SCons.Builder.Builder(
+            action = '$FAUST2SC --lang=sclang --prefix="${FAUST2SC_PREFIX}" -o $TARGET $SOURCES',
+            suffix = '.sc',
+            src_suffix = '.dsp.xml',
+            multi = True)
+    hs  = SCons.Builder.Builder(
+            action = '$FAUST2SC --lang=haskell --prefix="${FAUST2SC_HASKELL_MODULE}" -o $TARGET $SOURCES',
+            suffix = '.hs',
+            src_suffix = '.dsp.xml',
+            multi = True)
 
-    env.Append(BUILDERS = { 'Faust' : dsp,
-                            'FaustXML' : xml,
-                            'FaustSVG' : svg,
-                            'FaustSC' : sc })
+    env.Append(BUILDERS = { 'Faust'         : dsp,
+                            'FaustXML'      : xml,
+                            'FaustSVG'      : svg,
+                            'FaustSC'       : sc,
+                            'FaustHaskell'  : hs })
 
-    env['FAUST_ARCHITECTURE'] = 'module'
-    env['FAUST2SC'] = 'faust2sc'
-    env['FAUST2SC_PREFIX'] = ''
+    env['FAUST_ARCHITECTURE']       = 'module'
+    env['FAUST_FLAGS']              = []
+    env['FAUST_PATH']               = ['.', '/usr/local/lib/faust', '/usr/lib/faust']
+    env['FAUST2SC']                 = 'faust2sc'
+    env['FAUST2SC_PREFIX']          = ''
+    env['FAUST2SC_HASKELL_MODULE']  = ''
     
-    env.Append(SCANNERS = env.Scanner(function = faust_scan, skeys = ['.dsp']))
+    env.Append(SCANNERS = [
+        env.Scanner(function = dsp_source_scanner,
+                    skeys = ['.dsp'],
+                    path_function = SCons.Scanner.FindPathDirs('FAUST_PATH'))
+        ])
 
 def exists(env):
     return (env.WhereIs(FAUST) or SCons.Util.WhereIs(FAUST))
