97da466a50990a139842de841d33cbde09e9de52
stefan kersten <sk@k-hornz.de>
2009-07-08 18:47:54 +0200

    Add FM7 UGen function
    Ignore-this: 7397824db87a8a5ece5a0047fffefe3f
    
    darcs-hash:20090708164754-dde64-7f821975dc0a4fd4f51aedc502815201bfd1d48a.gz

--- haskell/Sound/SC3/UGen/SKUG/FM7.hs
+++ haskell/Sound/SC3/UGen/SKUG/FM7.hs
@@ -1,2 +1,21 @@
 module Sound.SC3.UGen.SKUG.FM7 (
+    fm7
 ) where
+
+import Sound.SC3.UGen hiding (fm7)
+
+listFrom3 :: (a, a, a) -> [a]
+listFrom3 (a1, a2, a3) = [a1, a2, a3]
+
+listFrom6 :: (a, a, a, a, a, a) -> [a]
+listFrom6 (a1, a2, a3, a4, a5, a6) = [a1, a2, a3, a4, a5, a6]
+
+type Control            = (UGen, UGen, UGen)
+type ControlMatrix      = (Control, Control, Control, Control, Control, Control)
+type Modulator          = (UGen, UGen, UGen, UGen, UGen, UGen)
+type ModulatorMatrix    = (Modulator, Modulator, Modulator, Modulator, Modulator, Modulator)
+
+fm7 :: ControlMatrix -> ModulatorMatrix -> UGen
+fm7 ctrlMatrix modMatrix = mkOsc AR "FM7" args 6
+    where args =    concat (map listFrom3 (listFrom6 ctrlMatrix))
+                 ++ concat (map listFrom6 (listFrom6 modMatrix))
